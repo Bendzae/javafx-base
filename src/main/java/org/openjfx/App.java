ee760561f5b72005ea37ef8d51f2b4b16f6432a7
Ben Dzaebel <bdzaebel@inovex.de>
2020-11-11 18:25:23 +0100

    some tweaks

--- src/main/java/org/openjfx/App.java
+++ src/main/java/org/openjfx/App.java
@@ -1,9 +1,12 @@
 package org.openjfx;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 
@@ -17,8 +20,18 @@ public class App extends Application {
         var javaVersion = SystemInfo.javaVersion();
         var javafxVersion = SystemInfo.javafxVersion();
 
+        //Root Element
+        var root = new VBox();
+        root.alignmentProperty().setValue(Pos.CENTER);
+
+        //Label
         var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        var scene = new Scene(new StackPane(label), 640, 480);
+
+        //Add label to Root Element
+        root.getChildren().addAll(label);
+
+        //Initialize & Show the Scene
+        var scene = new Scene(root, 640, 480);
         stage.setScene(scene);
         stage.show();
     }
